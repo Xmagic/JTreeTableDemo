a3d16e4ff1a8203e8a6bcb8ed08b6a15c1ebb292
James <gaojun1986@gmail.com>
2015-08-31 22:31:13 +0800

    Create project JTreeTableDemo

---
+++ src/com/xmagic/jtreetable/MyTreeTableSelectionModel.java
@@ -0,0 +1,29 @@
+package com.xmagic.jtreetable;
+
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.tree.DefaultTreeSelectionModel;
+
+public class MyTreeTableSelectionModel extends DefaultTreeSelectionModel
+{
+	
+	public MyTreeTableSelectionModel()
+	{
+		super();
+		
+		getListSelectionModel().addListSelectionListener(new ListSelectionListener()
+		{
+			@Override
+			public void valueChanged(ListSelectionEvent e)
+			{
+				
+			}
+		});
+	}
+	
+	ListSelectionModel getListSelectionModel()
+	{
+		return listSelectionModel;
+	}
+}
